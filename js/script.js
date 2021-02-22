738a048d39981b30377563e395a84dac0f736bbf
Angelo Lamacchia <angelo.lamacchia@live.it>
2021-02-22 18:11:18 +0100

    aggiunto controlli

--- js/script.js
+++ js/script.js
@@ -1,21 +1,33 @@
+// variabile tasto genera
 var genera = document.getElementById("genera");
-
+// variabile tasto annulla
 var annulla = document.getElementById("annulla");
 
 genera.addEventListener("click",
     function(){
+        // variabile nome
         var nome = document.getElementById("in_nome").value;
 
+        // variabile kilometri
         var kilometri = document.getElementById("in_kilometri").value;
+        if (kilometri > 1300 || kilometri == 0) {
+            alert("Valore Km non valido");
+            location.reload();
+        }
+
+        // costante prezzo al km
         const prezzoKm = (kilometri * 0.21);
 
+        // prezzario
         var prezzo = Math.round(prezzoKm*10)/10 + "€"
         var prezzoMin = Math.round((prezzoKm * 0.8)*10)/10 + "€";
         var prezzoOv = Math.round((prezzoKm * 0.6)*10)/10 + "€";
 
+        // variabile display block
         var display = document.getElementById("display");
         display.className = display.classList + "display_block"
 
+        // variabile tipo di prezzo
         var eta = document.getElementById("in_eta").value;
         if (eta == "Prezzo silver") {
             document.getElementById("out_prezzo").innerHTML = prezzoMin;
@@ -25,17 +37,24 @@ genera.addEventListener("click",
             document.getElementById("out_prezzo").innerHTML = prezzo;
         }
 
-        var genera = document.getElementById("genera");
+        // controllo nome
+        if (nome == "") {
+            alert("Inserire nome");
+            location.reload();
+        } else {
+            document.getElementById("out_nome").innerHTML = nome;
+        }
 
-        var annulla = document.getElementById("annulla");
-
-        document.getElementById("out_nome").innerHTML = nome;
-
+        // iniezione tipo di prezzo
         document.getElementById("out_sconto").innerHTML = eta;
 
-        document.getElementById("out_carrozza").innerHTML = Math.floor(Math.random()*10 +1);
+        // iniezione n carrozza
+        document.getElementById("out_carrozza").innerHTML = 
+        Math.floor(Math.random()*10 +1);
 
-        document.getElementById("out_codice").innerHTML = Math.floor(Math.random()*10000);
+        // iniezione codice
+        document.getElementById("out_codice").innerHTML = 
+        Math.floor(Math.random()*10000 +90000);
         
     }
 );
